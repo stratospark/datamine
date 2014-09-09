e9b05b45f79164c7e9e96ffe4fee239d4449b7c7
Patrick Rodriguez <patr84@gmail.com>
2014-09-08 20:22:48 -0700

    Added feathers package for REST/realtime API.

---
+++ app.js
@@ -0,0 +1,26 @@
+var feathers = require('feathers');
+var bodyParser = require('body-parser');
+
+var todoService = {
+    todos: [],
+
+    // Return all todos from this service
+    find: function(params, callback) {
+        callback(null, this.todos);
+    },
+
+    // Create a new Todo with the given data
+    create: function(data, params, callback) {
+        data.id = this.todos.length;
+        this.todos.push(data);
+
+        callback(null, data);
+    }
+};
+
+feathers()
+    .configure(feathers.rest())
+    .configure(feathers.socketio())
+    .use(bodyParser())
+    .use('/todos', todoService)
+    .listen(8000);
